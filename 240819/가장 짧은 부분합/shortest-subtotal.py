17fe970f129ef3debd9a9ce67672d273078014af
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-08-19 15:53:48 +0900

    Title: 가장 짧은 부분합, Time: 143ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240819/가장 짧은 부분합/shortest-subtotal.py
+++ 240819/가장 짧은 부분합/shortest-subtotal.py
@@ -3,10 +3,16 @@ MX = 987_654_321
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
-ans = MX 
+ans = MX
 sum_val = 0
+en = 0
 for st in range(n):
-    for en in range(st, n):
+    while en < n and sum_val + arr[en] < s:
         sum_val += arr[en]
+        en += 1
 
-        if sum_val >
+    ans = min(ans, en - st + 1)
+
+    sum_val -= arr[st]
+
+print(ans)
