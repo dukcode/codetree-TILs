ee50cb62a440c7b3971513cd7354319e99836978
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-08-20 13:43:58 +0900

    Title: 자연수면 출력, Time: 129ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240820/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a < 0) {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i = 0; i < b; ++i) {
+            System.out.print(a);
+        }
+    }
+}
