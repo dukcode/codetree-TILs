351ea5f43a6b8ce447ccd227241c7689dcab37ec
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-08-21 17:04:39 +0900

    Title: 두 정수에 대한 연산값 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240821/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240821/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    private static void solve(int[] arr) {
+        int minIdx = arr[0] < arr[1] ? 0 : 1;
+        int maxIdx = arr[0] > arr[1] ? 0 : 1;
+
+        arr[minIdx] += 10;
+        arr[maxIdx] *= 2;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int min = Math.min(a, b);
-        int max = Math.max(a, b);
+        int[] arr = {a, b};
+        solve(arr);
 
-        System.out.println((min + 10) + " " + (max * 2));
+        System.out.println(arr[0] + " " + arr[1]);
     }
 }
