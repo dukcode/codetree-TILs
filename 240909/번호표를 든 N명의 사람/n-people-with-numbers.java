5d77d90c44dcb284d2796e67c5bbcc452c05b6ce
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-09-09 12:02:12 +0900

    Title: 번호표를 든 N명의 사람, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240909/번호표를 든 N명의 사람/n-people-with-numbers.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  private static int n;
+  private static int maxT;
+  private static int[] times;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    input();
+    init();
+    int ans = solve();
+    bw.write(String.valueOf(ans));
+
+    br.close();
+    bw.close();
+  }
+
+  private static int solve() {
+    int minK = 0;
+    int maxK = n;
+
+    while (minK <= maxK) {
+      int halfK = (minK + maxK) / 2;
+
+      if (getTime(halfK) > maxT) {
+        minK = halfK + 1;
+      } else {
+        maxK = halfK - 1;
+      }
+
+    }
+
+    return minK;
+  }
+
+  private static int getTime(int k) {
+    int sumTime = 0;
+    for (int i = 0; i < n; ++i) {
+      sumTime += times[i];
+    }
+
+    return sumTime / k + sumTime % k;
+  }
+
+  private static void init() {
+    Arrays.sort(times);
+  }
+
+  private static void input() throws IOException {
+    st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    maxT = Integer.parseInt(st.nextToken());
+
+    times = new int[n];
+    for (int i = 0; i < n; i++) {
+      times[i] = Integer.parseInt(br.readLine());
+    }
+  }
+
+
+}
