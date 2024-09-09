de9d5122dc454fa63772d9f070babc7ba433712f
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-09-09 09:37:11 +0900

    Title: 최대 거리의 점, Time: 447ms, Memory: 22MB, Status: Passed - CodeTree

--- 240909/최대 거리의 점/maximum-distance-point.java
+++ 240909/최대 거리의 점/maximum-distance-point.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -30,6 +31,8 @@ public class Main {
     for (int i = 0; i < n; i++) {
       points[i] = Integer.parseInt(br.readLine());
     }
+    
+    Arrays.sort(points);
 
     bw.write(String.valueOf(solve()));
 
