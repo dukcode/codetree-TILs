6f399cd628cd25b39d4787842a7ed23ad53aa1f8
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-10-08 01:31:31 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241008/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  private static final int MX = 987_654_321;
+  private static final int MX_NUM = 100;
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  private static int n;
+  private static int[][] board;
+
+  private static int[][][] cache;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    n = Integer.parseInt(br.readLine());
+
+    board = new int[n][n];
+    for (int y = 0; y < n; y++) {
+      st = new StringTokenizer(br.readLine());
+      for (int x = 0; x < n; x++) {
+        board[y][x] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    cache = new int[MX_NUM + 1][n][n];
+    for (int k = 1; k <= MX_NUM; k++) {
+      for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+          cache[k][y][x] = MX;
+        }
+      }
+    }
+
+    bw.write(String.valueOf(solve()));
+
+    br.close();
+    bw.close();
+  }
+
+  public static int solve() {
+
+    for (int k = 1; k <= MX_NUM; k++) {
+      for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+          int minValue = Math.min(k, board[y][x]);
+
+          int upperMaxValue = inRange(y - 1, x) ? cache[k][y - 1][x] : MX;
+          int leftMaxValue = inRange(y, x - 1) ? cache[k][y][x - 1] : MX;
+
+          int selectedMaxValue = Math.min(leftMaxValue, upperMaxValue);
+          selectedMaxValue = selectedMaxValue == MX && k == board[y][x] ? k : selectedMaxValue;
+
+          int maxValue = Math.max(selectedMaxValue, board[y][x]);
+
+          cache[minValue][y][x] = Math.min(cache[minValue][y][x], maxValue);
+        }
+      }
+    }
+
+    int ret = MX;
+    for (int k = 1; k <= MX_NUM; k++) {
+      if (cache[k][n - 1][n - 1] == MX) {
+        continue;
+      }
+
+      ret = Math.min(ret, cache[k][n - 1][n - 1] - k);
+    }
+
+    return ret;
+  }
+
+  private static boolean inRange(int y, int x) {
+    return y >= 0 && y < n && x >= 0 && x < n;
+  }
+
+}
