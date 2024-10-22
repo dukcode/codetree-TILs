2d2d2a74730cb8edbba73b8ba1423a667e6d34ec
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-10-22 11:29:09 +0900

    Title: 올바른 등식 만들기, Time: 132ms, Memory: 9MB, Status: Passed - CodeTree

--- 241022/올바른 등식 만들기/right-equality.java
+++ 241022/올바른 등식 만들기/right-equality.java
@@ -51,11 +51,8 @@ public class Main {
     }
 
     if (idx == 0) {
-      if (res == 0) {
-        return 2;
-      }
       if (Math.abs(res) == arr[idx]) {
-        return 1;
+        return arr[idx] == 0 ? 2 : 1;
       }
 
       return 0;
