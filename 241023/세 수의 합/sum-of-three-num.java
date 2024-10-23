a5bea8cd908c6c938830c69533163fd7a2300355
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-10-23 11:54:44 +0900

    Title: 세 수의 합, Time: 460ms, Memory: 23MB, Status: Passed - CodeTree

--- 241023/세 수의 합/sum-of-three-num.java
+++ 241023/세 수의 합/sum-of-three-num.java
@@ -3,6 +3,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,6 +17,7 @@ public class Main {
   private static int k;
 
   private static int[] arr;
+  private static Map<Integer, Integer> freq;
 
   public static void main(String[] args) throws IOException {
     br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,23 +28,30 @@ public class Main {
     k = Integer.parseInt(st.nextToken());
 
     arr = new int[n];
+    freq = new HashMap<>();
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
+      int num = Integer.parseInt(st.nextToken());
+      arr[i] = num;
+      freq.put(num, freq.getOrDefault(num, 0) + 1);
     }
 
     int ans = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        for (int l = j + 1; l < n; l++) {
-          if (arr[i] + arr[j] + arr[l] == k) {
-            ans++;
-          }
+    for (int num : arr) {
+      freq.computeIfPresent(num, (k, v) -> v == 0 ? 0 : v - 1);
+      int counter = k - num;
+      for (int second : freq.keySet()) {
+        int third = counter - second;
+        if (third == second) {
+          ans += freq.get(second) * (freq.get(second) - 1);
+          continue;
         }
+
+        ans += freq.get(second) * freq.getOrDefault(third, 0);
       }
     }
 
-    bw.write(String.valueOf(ans));
+    bw.write(String.valueOf(ans / 2));
 
     br.close();
     bw.close();
