6b58634aab6db1125fabd056ddba25f0b38c15ac
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-10-23 10:31:57 +0900

    Title: 세 수의 합, Time: 348ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241023/세 수의 합/sum-of-three-num.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  private static int n;
+  private static int k;
+
+  private static int[] arr;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    st = new StringTokenizer(br.readLine());
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+
+    arr = new int[n];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        for (int l = j + 1; l < n; l++) {
+          if (arr[i] + arr[j] + arr[l] == k) {
+            ans++;
+          }
+        }
+      }
+    }
+
+    bw.write(String.valueOf(ans));
+
+    br.close();
+    bw.close();
+  }
+
+}
