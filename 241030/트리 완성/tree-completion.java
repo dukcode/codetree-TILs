1df274adc509f4f156ebfb0c0e57faa032a22ab5
Deok Yun Kim <59705184+dukcode@users.noreply.github.com>
2024-10-30 10:16:41 +0900

    Title: 트리 완성, Time: 3016ms, Memory: 23MB, Status: Time Limit Exceed - CodeTree

--- 241030/트리 완성/tree-completion.java
+++ 241030/트리 완성/tree-completion.java
@@ -44,7 +44,7 @@ public class Main {
     }
 
     for (int a = 0; a < n; a++) {
-      for (int b = 0; b < n; b++) {
+      for (int b = a + 1; b < n; b++) {
         int rootA = findRoot(a);
         int rootB = findRoot(b);
         if (rootA == rootB) {
